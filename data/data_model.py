51b646a0f3b5f13f5528a9b4e1302aea4bafe09f
Manish-rai21bit <manish.rai21bit@gmail.com>
2018-08-02 00:41:29 -0500

    dicttojson and jsontodict function correction

--- data/data_model.py
+++ data/data_model.py
@@ -1,9 +1,8 @@
 
 def fetchcolvalue(filename, searchkey, output_col_no):
-    csvfile = open(os.path.join(Project_filepath, filename), 'rb')
+    csvfile = open(os.path.join(Project_filepath, filename), 'r')
     csvdata = csv.reader(csvfile, delimiter=',')
     first_row = next(csvdata)
-    #filename = open(kids_agefile, 'r')
     ct = 0
     for row in csvdata:
         if searchkey in row[ct]:
@@ -17,7 +16,7 @@ def fetchcolvalue(filename, searchkey, output_col_no):
 
 # URL fetching
 def fetchurl(filename, searchkey):
-    csvfile = open(os.path.join(Project_filepath, filename), 'rb')
+    csvfile = open(os.path.join(Project_filepath, filename), 'r')
     csvdata = csv.reader(csvfile, delimiter=',')
     first_row = next(csvdata)
     csvdata2 = sorted(csvdata, key=lambda row: (0, 1)) 
@@ -34,7 +33,7 @@ def fetchurl(filename, searchkey):
 def csvtodict(Project_filepath, bb_data, concensus_data, all_images_data, images):
     lst = []
     event_dict = {}
-    csvfile = open(os.path.join(Project_filepath, 'Data/Schneider_Data/TestGoldStandardBoundBoxCoord.csv'), 'rb')
+    csvfile = open(os.path.join(Project_filepath, bb_data), 'r')
     csvdata = csv.reader(csvfile, delimiter=',')
     first_row = next(csvdata)
     for row in csvdata:
@@ -42,7 +41,7 @@ def csvtodict(Project_filepath, bb_data, concensus_data, all_images_data, images
             event_dict[row[0][0:10]] = {'metadata' : {"SiteID": fetchcolvalue(concensus_data, row[0][0:10], 3), 
                                   "DateTime": fetchcolvalue(concensus_data, row[0][0:10], 2),  
                                   "Season": fetchcolvalue(all_images_data, row[0][0:10], 1)[0:2]},
-                                    'images' : [{"Path" : os.path.join(Project_filepath, 'Data/Schneider_Data/', row[0]),
+                                    'images' : [{"Path" : os.path.join(Project_filepath, 'Data/Schneider_Data/images/', row[0]),
                                 "URL" : fetchurl(all_images_data, row[0][0:10]),
                                 "dim_x" : fetchcolvalue(bb_data, row[0], 1),
                                 "dim_y" : fetchcolvalue(bb_data, row[0], 2),
@@ -51,32 +50,39 @@ def csvtodict(Project_filepath, bb_data, concensus_data, all_images_data, images
                                       "bb_ymin" : fetchcolvalue(bb_data, row[0], 5),
                                       "bb_xmax" : fetchcolvalue(bb_data, row[0], 6),
                                       "bb_ymax" : fetchcolvalue(bb_data, row[0], 7), 
-                                      "bb_label" : fetchcolvalue(bb_data, row[0], 3)
+                                      "bb_primary_label" : fetchcolvalue(bb_data, row[0], 3), 
+                                      "bb_label" : {"species" : fetchcolvalue(bb_data, row[0], 3),
+                                                    "pose" : "standing/ sitting/ running"
+                                                   }
                                      }]
                                }]
                                     }
     return event_dict
 
+
 def dicttojson(event_dict):
-    return json.dumps(event_dict)
+    with open('event_dict.json', 'w') as outfile:
+        json.dump(event_dict, outfile)
+    #return json.dump(event_dict, outfile)
 
 def jsontodict(event_json):
-    return json.loads(event_json)
+    with open(event_json, 'r') as f:
+        return json.load(f)
 
 
 def create_tf_example(data):
     # TODO(user): Populate the following variables from your example.
-    height = int(data['dim_y']) # Image height
-    width = int(data['dim_x']) # Image width
-    filename = str(data['Path']) # Filename of the image. Empty if image is not from file
-    #encoded_image_data = None # Encoded image bytes
+    height = int(data['images'][0]['dim_y']) # Image height
+    width = int(data['images'][0]['dim_x']) # Image width
+    filename = str(data['images'][0]['Path']) # Filename of the image. Empty if image is not from file
+    encoded_image_data = resize_jpeg(str(data['images'][0]['Path']),  10) # Encoded image bytes
     image_format = b'jpg' # b'jpeg' or b'png'
 
-    xmins = [float(data['observations'][0]['bb_xmin'])] # List of normalized left x coordinates in bounding box (1 per box)
-    xmaxs = [float(data['observations'][0]['bb_xmax'])] # List of normalized right x coordinates in bounding box
+    xmins = [float(data['images'][0]['observations'][0]['bb_xmin'])] # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [float(data['images'][0]['observations'][0]['bb_xmax'])] # List of normalized right x coordinates in bounding box
              # (1 per box)
-    ymins = [float(data['observations'][0]['bb_ymin'])] # List of normalized top y coordinates in bounding box (1 per box)
-    ymaxs = [float(data['observations'][0]['bb_ymax'])] # List of normalized bottom y coordinates in bounding box
+    ymins = [float(data['images'][0]['observations'][0]['bb_ymin'])] # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [float(data['images'][0]['observations'][0]['bb_ymax'])] # List of normalized bottom y coordinates in bounding box
              # (1 per box)
     #classes_text = data[''] # List of string class name of bounding box (1 per box)
     #classes = data[''] # List of integer class id of bounding box (1 per box)
@@ -86,7 +92,7 @@ def create_tf_example(data):
       'image/width': dataset_util.int64_feature(width),
       'image/filename': dataset_util.bytes_feature(filename),
       'image/source_id': dataset_util.bytes_feature(filename),
-      #'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+      'image/encoded': dataset_util.bytes_feature(encoded_image_data),
       'image/format': dataset_util.bytes_feature(image_format),
       'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
       'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
@@ -99,8 +105,8 @@ def create_tf_example(data):
     return tf_example
 
 
-def encode_to_tfr_record(test_feature):
-    writer = tf.python_io.TFRecordWriter('test_event.tfrecord')
-    example = create_tf_example(test_feature)
-    writer.write(example.SerializeToString())
-    writer.close()
+def encode_to_tfr_record(test_feature, out_tfr_file):
+    with tf.python_io.TFRecordWriter(out_tfr_file) as writer:
+        for k, v in test_feature.items():
+            example = create_tf_example(v)
+            writer.write(example.SerializeToString())
