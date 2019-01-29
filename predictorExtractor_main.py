f0805d0934220f10ee88006823ba619134b018a5
manish.rai21bit <manish.rai21bit@gmail.com>
2019-01-29 17:12:26 +0000

    predictor extractor from TFRecord

---
+++ predictorExtractor_main.py
@@ -0,0 +1,29 @@
+"""Main function for extracting TFRecord to a csv. 
+Uses the helper modules predictor_extractor.py"""
+
+import argparse
+
+from data_prep.predictor_extractor import predictorExtractor
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--tfrecord_path_list", nargs='+', type=str, required=True,
+        help="Path to TFRecord files")
+    parser.add_argument(
+        "--output_csv", type=str, required=True,
+        help="output csv file"
+        )
+    parser.add_argument(
+        "--batch_size", type=int, default=512,
+        help="batch size")
+    parser.add_argument(
+        "--score_threshold", type=float, default=0.5,
+        help="score thresholds to write to csv")
+
+
+
+    kwargs = vars(parser.parse_args())
+
+    predictorExtractor(**kwargs)
