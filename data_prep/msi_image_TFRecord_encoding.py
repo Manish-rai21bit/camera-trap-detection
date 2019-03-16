b7e924637daf07e33f75ff59f9b6b93420153dea
Manish Kumar Rai <rai00007@login02.msi.umn.edu>
2019-03-16 00:11:53 -0500

    added feature to have tfrecord shards instead of a big file

--- data_prep/msi_image_TFRecord_encoding.py
+++ data_prep/msi_image_TFRecord_encoding.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 17 11:53:44 2019
 
-@author: manishrai
-"""
 
 import os, sys, csv
 import tensorflow as tf
 import dataset_util
 from PIL import ImageFile
+# imports for sharding
+import contextlib2
+from object_detection.dataset_tools import tf_record_creation_util
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 """This function reads a raw image, resizes it with aspect ratio preservation and returns the byte string"""
@@ -54,26 +52,35 @@ def create_tf_example(data_dict):
     serializes the tfrecord examples and writes to a tfrecord file!!!
     As of now, it saves the TFRecord file in the home directory where the code is executed"""
 def encode_to_tfr_record(test_feature, out_tfr_file):
-    with tf.python_io.TFRecordWriter(out_tfr_file) as writer:
-        count = 0
-        for v in test_feature:
-            count+=1
-            if count%500==0:
-                print("processing event number %s : %s" % (count, v))
-            example = create_tf_example(v)
-            writer.write(example.SerializeToString())
+    num_shards=100
+    with contextlib2.ExitStack() as tf_record_close_stack:
+        output_tfrecords = tf_record_creation_util.open_sharded_output_tfrecords(
+            tf_record_close_stack, out_tfr_file, num_shards)
+        for index, example in enumerate(test_feature):
+            tf_example = create_tf_example(example)
+            output_shard_index = index % num_shards
+            output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
+
+    # with tf.python_io.TFRecordWriter(out_tfr_file) as writer:
+       #  count = 0
+        # for v in test_feature:
+            # count+=1
+            # if count%500==0:
+                # print("processing event number %s : %s" % (count, v))
+            # example = create_tf_example(v)
+            # writer.write(example.SerializeToString())
 
 
 def main():
-    with open("/home/packerc/rai00007/camera-trap-detection/data/LILA/msi_snapshot_serengeti.csv",'r') as f:
+    with open("/home/packerc/rai00007/camera-trap-detection/data/LILA/msi_snapshot_serengeti_new.csv",'r') as f:
         l = []
         rd = csv.reader(f)
         for val in rd:
             l.append(val)
        
     event_dict = l[0]
-    event_dict1 = event_dict[20000:50000]
-    encode_to_tfr_record(event_dict1, 'snapshot_serengeti_s01_s06-20000-50000.record')
+    event_dict1 = event_dict[600000:]
+    encode_to_tfr_record(event_dict1, 'TFrecords/snapshot_serengeti_s01_s06_600000_end.record')
 
 if  __name__=='__main__':
     # event_dict = ['S1/B04/B04_R1/S1_B04_R1_PICT0040']
