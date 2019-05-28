c1bf5f938db9c0f3dab2ae5ce7d05646a7c8d352
manish.rai21bit <manish.rai21bit@gmail.com>
2019-05-28 20:41:57 +0000

    Consolidating he visualization scripts into tfr_visualization.py

--- data_prep/tfr_visualization.py
+++ data_prep/tfr_visualization.py
@@ -1,6 +1,8 @@
 
 """Visualization API for:
-1. Predictions
+1. Raw images from the MSI. These images have been encoded to TF Record
+2. Predictions made by the infer_detection.py using the model
+3. Training TFRecords that has been encoded by TF Record
 """
 
 import tensorflow as tf
@@ -19,8 +21,27 @@ sys.path.append('/home/ubuntu/data/tensorflow/my_workspace/camera-trap-detection
 from data.utils import dataset_util
 
 
+# Decoding the raw images
+def decode_record_raw_image(serialized_example):
+    """Decode record for the raw images."""
+    context_features = {
+                        'image/filename': tf.FixedLenFeature([], tf.string),
+                        'image/encoded': tf.FixedLenFeature([], tf.string),
+                        'image/format': tf.FixedLenFeature([], tf.string)
+                    }
+
+
+    context, sequence = tf.parse_single_sequence_example(serialized=serialized_example,
+                                              context_features=context_features,
+#                                               sequence_features=sequence_features,
+                                              example_name=None,
+                                              name=None)
+
+    return ({k: v for k, v in context.items()},{k: v for k, v in sequence.items()})
+
+# Decoding Detection TFRecord
 def decode_record_pred(serialized_example):
-    """Decode the TFRecord data for each example. """
+    """Decode the TFRecord data for each example from the detections. """
     context_features = {
                         'image/filename': tf.FixedLenFeature([], tf.string),
                         'image/encoded': tf.FixedLenFeature([], tf.string),
@@ -42,9 +63,73 @@ def decode_record_pred(serialized_example):
 
     return ({k: v for k, v in context.items()},{k: v for k, v in sequence.items()})
 
+# Decoding Training TFRecord
+def decode_record_training_image(serialized_example):
+    """Decode the TFRecord data for each example from the training TFRecord. """
+    context_features = {
+                        'image/height': tf.FixedLenFeature([], tf.int64),
+                        'image/width': tf.FixedLenFeature([], tf.int64),
+                        'image/filename': tf.FixedLenFeature([], tf.string),
+                        'image/source_id': tf.FixedLenFeature([], tf.string),
+                        'image/encoded': tf.FixedLenFeature([], tf.string),
+                        'image/format': tf.FixedLenFeature([], tf.string),
+                        "image/object/bbox/xmin" : tf.VarLenFeature(tf.float32),
+                        "image/object/bbox/xmax" : tf.VarLenFeature(tf.float32),
+                        "image/object/bbox/ymin" : tf.VarLenFeature(tf.float32),
+                        "image/object/bbox/ymax" : tf.VarLenFeature(tf.float32),
+                        "image/object/class/text" : tf.VarLenFeature(tf.string),
+                        "image/object/class/label" : tf.VarLenFeature(tf.int64)
+                    }
 
-def plot_images_with_bbox_pred(filename_list, outfile, inv_label_map, num_batches=1, 
-                          score_threshold=0.5, batch_size= 2):
+
+    context, sequence = tf.parse_single_sequence_example(serialized=serialized_example,
+                                              context_features=context_features,
+#                                               sequence_features=sequence_features,
+                                              example_name=None,
+                                              name=None)
+
+    return ({k: v for k, v in context.items()},{k: v for k, v in sequence.items()})
+
+
+# Plot the raw images
+def plot_images_raw(filename_list, 
+                    outfile, 
+                    num_batches=1, 
+                    batch_size= 2):
+    """Plot n raw images and save it in the location given.
+    filename_list : list of TFRecords
+    """
+
+    # Create a tensorflow dataset
+    dataset = tf.data.Dataset.from_tensor_slices(filename_list)
+    dataset = tf.data.TFRecordDataset(dataset)
+    dataset = dataset.shuffle(buffer_size=batch_size)
+    dataset = dataset.map(lambda x: decode_record_raw_image(serialized_example=x)).batch(batch_size)
+
+    for i, (context, sequence) in enumerate(dataset):
+        if i<num_batches:
+            for j in range(batch_size):
+                img = context['image/encoded'][j]
+                encoded_jpg_io = io.BytesIO(img.numpy())
+                image = Image.open(encoded_jpg_io)
+                width, height = image.size
+                # Create figure and axes
+                fig,ax = plt.subplots(1)
+                fig.set_size_inches(10, 8)
+                # Display the image
+                ax.set_title(context['image/filename'][j].numpy().decode('utf-8'))
+                ax.imshow(image)
+                fig.savefig(os.path.join(outfile, context['image/filename'][j].numpy().decode('utf-8').split('/')[-1]))
+                plt.clf
+
+
+# Plot the boxes for Detection TFRecord
+def plot_images_with_bbox_pred(filename_list, 
+                               outfile, 
+                               inv_label_map, 
+                               num_batches=1, 
+                               score_threshold=0.5, 
+                               batch_size= 2):
     """Plot n images with bounding boxes and save it in the location givenself.
     filename_list : list of TFRecords
     """
@@ -102,6 +187,67 @@ def plot_images_with_bbox_pred(filename_list, outfile, inv_label_map, num_batche
                     cy = ry # + rect.get_height()/2.0
 
                     ax.annotate((inv_label_map[labels_d[s]], str(scores[s])+str('%')), (cx, cy), color='b', weight='bold', 
+                                fontsize=8, ha='left', va='top') 
+                fig.savefig(os.path.join(outfile, filename[rec_i].numpy().decode('utf-8').split('/')[-1]))
+                plt.clf
+                
+# Plot the boxes for Training TFRecord
+def plot_images_training_with_bbox(filename_list, 
+                                   outfile, 
+                                   inv_label_map, 
+                                   num_batches=1, 
+                                   batch_size= 2):
+    """Plot n raw images and save it in the location givenself.
+    filename_list : list of TFRecords
+    """
+
+    # Create a tensorflow dataset
+    dataset = tf.data.Dataset.from_tensor_slices(filename_list)
+    dataset = tf.data.TFRecordDataset(dataset)
+    dataset = dataset.shuffle(buffer_size=batch_size)
+    dataset = dataset.map(lambda x: decode_record_training_image(serialized_example=x)).batch(batch_size)
+
+    for i, (context, sequence) in enumerate(dataset):
+        if i<num_batches:
+            batch_shape = context['image/object/bbox/xmin'].dense_shape
+            filename = context['image/filename']
+            xmin = tf.sparse_tensor_to_dense(context['image/object/bbox/xmin'])
+            ymin = tf.sparse_tensor_to_dense(context['image/object/bbox/ymin'])
+            xmax = tf.sparse_tensor_to_dense(context['image/object/bbox/xmax'])
+            ymax = tf.sparse_tensor_to_dense(context['image/object/bbox/ymax'])
+            label = tf.sparse_tensor_to_dense(context['image/object/class/label'])
+            
+            for rec_i in range(0, int(batch_shape[0])):
+                xmins, ymins, xmaxs, ymaxs, labels = [], [], [], [], []
+
+                img = context['image/encoded'][rec_i]
+                encoded_jpg_io = io.BytesIO(img.numpy())
+                image = Image.open(encoded_jpg_io)
+                width, height = image.size
+                
+                for box_i in range(0, int(batch_shape[1])):   
+                    xmins.append((xmin[rec_i, box_i].numpy())*width)
+                    ymins.append((ymin[rec_i, box_i].numpy())*height)
+                    xmaxs.append((xmax[rec_i, box_i].numpy())*width)
+                    ymaxs.append((ymax[rec_i, box_i].numpy())*height)
+                    labels.append(int(label[rec_i, box_i].numpy()))
+                    
+                # Create figure and axes
+                fig,ax = plt.subplots(1)
+                fig.set_size_inches(10, 8)
+                # Display the image
+                ax.set_title(filename[rec_i].numpy().decode('utf-8'))
+                ax.imshow(image)
+                for s in range(len(xmins)):
+                    if int(labels[s]) != 0:
+                        rect = patches.Rectangle((xmins[s],ymins[s]),(xmaxs[s]-xmins[s]), \
+                                                 (ymaxs[s] - ymins[s]),linewidth=2,edgecolor='b',facecolor='none')
+                        ax.add_patch(rect)
+                        rx, ry = rect.get_xy()
+                        cx = rx # + rect.get_width()/2.0
+                        cy = ry # + rect.get_height()/2.0
+
+                        ax.annotate(inv_label_map[labels[s]], (cx, cy), color='b', weight='bold', 
                                 fontsize=8, ha='left', va='top') 
                 fig.savefig(os.path.join(outfile, filename[rec_i].numpy().decode('utf-8').split('/')[-1]))
                 plt.clf
--- tfr_visualization_main.py
+++ tfr_visualization_main.py
@@ -25,16 +25,22 @@ if __name__ == '__main__':
         help="output directory of the image to be saved"
         )
     parser.add_argument(
-    "--label_map_json", type=str, required=True,
-    help="label map json"
+        "--label_map_json", type=str, required=True,
+        help="label map json"
     )
     parser.add_argument(
         "--num_batches", type=int, default=1,
         help="number of batches to save. batch size = 2"
     )
     parser.add_argument(
-    "--score_threshold", type=float, default=0.5,
-    help="threshold of the detected box to be plotted"
+        "--score_threshold", type=float, default=0.5,
+        help="threshold of the detected box to be plotted"
+    )
+    parser.add_argument(
+        "--TFRecord_type", type=str, required=True,
+        help="Type of the TF Record. \
+             Pred: for TFRecord with Predictions, \
+             Train: for TFRecord with Training dataset"
     )
 
     args = parser.parse_args()
@@ -42,5 +48,19 @@ if __name__ == '__main__':
     with open(args.label_map_json, 'r') as f:
          label_map = json.load(f)
     inv_label_map = {v: k for k, v in label_map.items()}
-        
-    visual.plot_images_with_bbox_pred(args.filename_list, args.outfile, inv_label_map, args.num_batches, score_threshold = args.score_threshold)
+    
+    if args.TFRecord_type=='Pred':
+        visual.plot_images_with_bbox_pred(args.filename_list, 
+                                          args.outfile, 
+                                          inv_label_map, 
+                                          args.num_batches, 
+                                          score_threshold = args.score_threshold)
+    elif args.TFRecord_type=='Train':
+        visual.plot_images_training_with_bbox(args.filename_list, 
+                                          args.outfile, 
+                                          inv_label_map, 
+                                          args.num_batches)
+    elif args.TFRecord_type=='Raw':
+        visual.plot_images_raw(args.filename_list, 
+                               args.outfile,
+                              args.num_batches)
